4fd4913e5afd95361cc494c2a9111b97639f5e7c
vasiliy1999 <vkolyasev1999@mail.ru>
2020-05-08 14:54:09 +0300

    Regestration

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 
+session_start();
+$connection = new PDO('mysql:host=localhost; dbname=site;
+charset=utf8', 'root', 'root');
+if ($_POST['login']) {
+    $newName = $_POST['name'];
+    $newSurname = $_POST['surname'];
+    $newYear = $_POST['year'];
+    $newCity = $_POST['city'];
+    $newEmail = $_POST['email'];
+    $newLogin = $_POST['login'];
+    $newPassword = $_POST['password'];
+    //$newPassword1 = $_POST['password1'];
+    //if ($newPassword == $newPassword1) {
+        $connection->query("INSERT INTO login(name, surname, year, city, email, login, password) 
+            VALUES ('$newName', '$newSurname', '$newYear', '$newCity', '$newEmail', '$newLogin', '$newPassword')");
+    header('Location: index.php');
+    //}
+   /* else {
+        echo 'Введенные пароли не совпадают';
+        header('Location: index.php');
+    }*/
+}
+
 ?>
 
 <form action="" method="post">
@@ -10,6 +33,7 @@
     <input type="email" name="email" required placeholder="Почта"><br> <br>
     <input type="login" name="login" required placeholder="Придумайте логин"><br> <br>
     <input type="password" name="password" required placeholder="Придумайте пароль"><br>
-    <input type="password1" name="password1" required placeholder="Повторите пароль"><br>
+    <!--<input type="password1" name="password1" required placeholder="Повторите пароль"><br>-->
     <input type="submit" value="Зарегестрироваться" style="color: blue">
+
 </form>
