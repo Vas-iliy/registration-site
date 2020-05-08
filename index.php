e0191002eb680e7232c86ba1d50a6577f4c8cb2e
vasiliy1999 <vkolyasev1999@mail.ru>
2020-05-08 13:25:11 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<form action="" method="post">
+    <input type="name" name="name" required placeholder="Имя"> <br> <br>
+    <input type="surname" name="surname" required placeholder="Фамилия"><br> <br>
+    <input type="year" name="year" required placeholder="Год Рождения"><br> <br>
+    <input type="city" name="city" required placeholder="Город"><br> <br>
+    <input type="email" name="email" required placeholder="Почта"><br> <br>
+    <input type="login" name="login" required placeholder="Придумайте логин"><br> <br>
+    <input type="password" name="password" required placeholder="Придумайте пароль"><br>
+    <input type="password1" name="password1" required placeholder="Повторите пароль"><br>
+    <input type="submit" value="Зарегестрироваться" style="color: blue">
+</form>
